06dea46e4b609c177c74d22f1c8fff53cbdce342
Zachary Kurtz <me@Zacharys-MacBook-Pro.local>
2019-04-12 21:12:07 -0400

    try again to let pypi parse readme

--- setup.py
+++ setup.py
@@ -2,11 +2,16 @@ from setuptools import setup
 
 exec(open("shmistogram/version.py").read())
 
-try:
-    import pypandoc
-    long_description = pypandoc.convert('README.md', 'rst')
-except(IOError, ImportError):
-    long_description = open('README.md').read()
+# try:
+#     import pypandoc
+#     long_description = pypandoc.convert_file('README.md', 'rst')
+# except(IOError, ImportError):
+#     long_description = open('README.md').read()
+
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name='shmistogram',
@@ -17,6 +22,7 @@ setup(
     url="https://github.com/zkurtz/shmistogram",
     license='MIT',
     long_description=long_description,
+    long_description_content_type='text/markdown',
     packages=[
         'shmistogram',
         'shmistogram.binners',

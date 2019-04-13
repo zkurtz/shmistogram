f6bc57e5c4299f6b1a5bb451171fff0858c08787
Zachary Kurtz <me@Zacharys-MacBook-Pro.local>
2019-04-12 20:40:37 -0400

    setup

--- setup.py
+++ setup.py
@@ -2,9 +2,21 @@ from setuptools import setup
 
 exec(open("shmistogram/version.py").read())
 
+try:
+    import pypandoc
+    long_description = pypandoc.convert('README.md', 'rst')
+except(IOError, ImportError):
+    long_description = open('README.md').read()
+
 setup(
     name='shmistogram',
     version=__version__,
+    author="Zach Kurtz",
+    author_email="zkurtz@gmail.com",
+    description="Piecewise-uniform univariate density estimation and visualization",
+    url="https://github.com/zkurtz/shmistogram",
+    license='MIT',
+    long_description=long_description
     packages=[
         'shmistogram',
         'shmistogram.binners',
@@ -17,5 +29,4 @@ setup(
         'pandas',
         'scipy',
     ],
-    license='See LICENSE.txt',
 )

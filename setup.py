0bc7222bf55fad2e87fc0879a096f499804a9f6e
Zachary Kurtz <me@Zacharys-MacBook-Pro.local>
2019-04-13 09:30:34 -0400

    correct install travis

--- setup.py
+++ setup.py
@@ -7,8 +7,8 @@ this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
-with open('requirements.txt') as f:
-    requirements = f.read().splitlines()
+# with open('requirements.txt') as f:
+#     requirements = f.read().splitlines()
 
 setup(
     name='shmistogram',

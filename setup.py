6fb63e97f2761354670f8fc9fec527fa94ab4475
Zachary Kurtz <me@Zacharys-MacBook-Pro.local>
2019-04-13 08:51:48 -0400

    experiment with unifying requirements and install_requires

--- setup.py
+++ setup.py
@@ -2,16 +2,13 @@ from setuptools import setup
 
 exec(open("shmistogram/version.py").read())
 
-# try:
-#     import pypandoc
-#     long_description = pypandoc.convert_file('README.md', 'rst')
-# except(IOError, ImportError):
-#     long_description = open('README.md').read()
-
 from os import path
 this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
 
 setup(
     name='shmistogram',
@@ -29,10 +26,5 @@ setup(
         'shmistogram.plot',
         'shmistogram.simulations'
     ],
-    install_requires=[
-        'astropy',
-        'matplotlib',
-        'pandas',
-        'scipy',
-    ],
+    install_requires=requirements
 )
